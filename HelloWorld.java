60dd9f8419ee800932bd4aadde014b4c10f0c208
Jismely <jcasta15@lab.cpcc.edu>
2025-02-15 13:51:55 -0500

    Revert "Name and Age"
    This reverts commit 4d09038b153859ed964fe41a1386c24c86f47746.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,18 +1,7 @@
 public class HelloWorld
 {
-   public static void main(String[] args)
+   public class static void main(String[] args)
    {
       System.out.println("Hello world!");
-      
-      //name is a reference variable
-      String name = "George Washington";
-      
-      System.out.println(name);
-      
-      //age is a primitive variable
-      int age = 24;
-      
-      System.out.println(age);
-      
-   }//end main
-}//end class
+   }
+}
